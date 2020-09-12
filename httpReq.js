497a58b397ec01e19707a31d6822fad85a94f477
Sandesh Timilsina <stimilsina@unm.edu>
2020-09-12 16:56:24 -0600

    covid data- fetch changed

--- httpReq.js
+++ httpReq.js
@@ -12,7 +12,10 @@
 const  api_url = 'https://covidtracking.com/api/v1/states/daily.json';
 let dataGlobal = null;
 async  function getData() {
-    const response = await fetch(api_url);
+    const response = await fetch(api_url, {
+        mode: 'cors',
+        credentials:'include'
+    });
     dataGlobal = await response.json();
     await dataGlobal;
     for (let i = 0; i<50; i++){
