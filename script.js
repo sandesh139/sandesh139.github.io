4532317b6a150980aef2d4c4fc73c9fad429ace2
Sandesh <timilsinasandesh7@gmail.com>
2020-11-02 20:23:17 -0700

    added the sliding navigation

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 
 
 const toggle = document.getElementById('toggle');
+document.body.classList.toggle('show-nav');
+document.body.classList.toggle('change');
 
 toggle.addEventListener('click', ()=> {
     document.body.classList.toggle('show-nav');
