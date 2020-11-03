7faf08cca108ced385da07ab893235bf1c9f2f3f
Sandesh <timilsinasandesh7@gmail.com>
2020-11-02 20:15:07 -0700

    added the sliding navigation

---
+++ script.js
@@ -0,0 +1,22 @@
+// const showRulesButton = document.getElementById('toggleID');
+
+
+
+
+
+
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('click', ()=> {
+    document.body.classList.toggle('show-nav');
+    document.body.classList.toggle('change');
+}
+);
+
+
+const toggleV = document.getElementById('toggleV');
+toggleV.addEventListener('click', ()=> {
+    document.body.classList.toggle('show-nav');
+    document.body.classList.toggle('change');
+}
+);
